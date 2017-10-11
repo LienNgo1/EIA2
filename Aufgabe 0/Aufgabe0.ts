ae5513d08c329bde2ad8e51504d9784da6ba0aca
Lien <Lien@192.168.0.80>
2017-10-11 15:51:29 +0200

    Aufgabe 0

--- Aufgabe 0/Aufgabe0.ts
+++ Aufgabe 0/Aufgabe0.ts
@@ -3,9 +3,10 @@
 namespace Aufgabe0 {
 
 var person = prompt("Please enter your name", "Harry Potter");
-
 if (person != null) {
     document.getElementById("demo").innerHTML =
     "Hello " + person + "! How are you today?";
+    
+    
 } 
 }
