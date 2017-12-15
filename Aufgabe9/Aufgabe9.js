137edac2fc630d5f06c81e97fe8b90247003bfe3
Lien <Lien@this-air.hfu-open.fu.hs-furtwangen.de>
2017-12-15 14:44:57 +0100

    Test

--- Aufgabe9/Aufgabe9.js
+++ Aufgabe9/Aufgabe9.js
@@ -1,4 +1,4 @@
-/* Aufgabe 8: RandomBoxes
+/* Aufgabe : Erpresserbrief
 Name: Ngo, Thi Lien
 Matrikel: 256778
 Datum: 8.12.2017
@@ -6,9 +6,11 @@ Datum: 8.12.2017
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 
 */
-var Aufgabe8;
-(function (Aufgabe8) {
+var Aufgabe9;
+(function (Aufgabe9) {
     window.addEventListener("load", init);
+    //Globale Variable anlegen. -wie kann ich die versch. Div's vergleichen?
+    let currentLetter;
     function init() {
         let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"];
         let anzahl = alphabet.length;
@@ -17,17 +19,79 @@ var Aufgabe8;
                 drawRect(alphabet[i]);
             }
         }
+        drawKasten();
     }
+    //handle click wird immmer aufgerufen, wenn mans anklickt 
+    //Event.target gibt dir das objekt zurück - liefert div zurück
     //Funktion Kästchen Zeichnen
     function drawRect(_alphabet) {
         let div = document.createElement("div"); //div erstellen
         div.style.width = "40px"; //Returns a String of an object
         div.style.height = "40px";
         div.style.backgroundColor = "red";
-        div.innerText = _alphabet;
+        div.innerText = _alphabet; // 
         div.style.marginLeft = "2px";
-        div.style.marginTop = "2px";
+        div.style.marginTop = "2px"; //
+        div.id = _alphabet;
+        div.className = "letters";
+        div.addEventListener("click", handleClick);
         document.body.appendChild(div); //html ELement in den Code einfügen lassen - hängt div an body dran - wird gezeichnet
     }
-})(Aufgabe8 || (Aufgabe8 = {}));
+    function drawKasten() {
+        let letter = document.createElement("div");
+        letter.style.width = "800px"; //Returns a String of an object
+        letter.style.height = "300px";
+        letter.style.backgroundColor = "grey";
+        letter.style.marginLeft = "2px";
+        letter.style.marginTop = "2px"; //
+        letter.addEventListener("click", putLetter);
+        document.body.appendChild(letter);
+    }
+    function handleClick(_event) {
+        let click = _event.target;
+        click.style.backgroundColor = "blue";
+        currentLetter = click.id;
+        let divlist = document.getElementsByClassName("letters");
+        for (let i = 0; i < divlist.length; i++) {
+            if (currentLetter != divlist[i].id) {
+                divlist[i].style.backgroundColor = "blue";
+            }
+        }
+    }
+    /*function handleClick(_event: MouseEvent): void{
+        let click: HTMLDivElement = <HTMLDivElement>_event.target;
+        click.style.backgroundColor = "black";
+        //property vom click verändert = blacl´k
+        currentletter = click.id;
+        
+        let divlist: NodeListOf<HTMLDivElement> = <NodeListOf<HTMLDivElement>> document.getElementsByClassName("letters");
+        
+        // divlist ist von type nodelist of htmldiv element - hinter dem doppelpunkt: datentyp
+        // nodelistof ... wie ien array ... kann viele versch. datentyp speichern
+        //get element by clas name...
+        
+        for(let i: number = 0; i<divlist.length; i++)<{
+         if(currentLetter != divlist[i].idy    //wenn der Buchstabe "currentletter" nicht gleich divlist ist der angeklickt ist
+            divlist[i].style.backgroundColor = "blue"; //dann wird der Buchstabe wieder blau
+       }
+    }
+    
+    */
+    function putLetter(_event) {
+        let box = document.createElement("div");
+        box.style.width = "30px";
+        box.style.height = "30px";
+        box.style.backgroundColor = "blue";
+        box.innerText = currentLetter;
+        box.style.marginLeft = "3px";
+        box.style.marginTop = "3px";
+        box.id = currentLetter;
+        box.className = "letters";
+        box.style.position = "absolute";
+        box.style.left = _event.pageX + "px";
+        box.style.top = _event.pageY + "px";
+        document.body.appendChild(box);
+        let clicking = _event.target;
+    }
+})(Aufgabe9 || (Aufgabe9 = {}));
 //# sourceMappingURL=Aufgabe9.js.map
--- Aufgabe9/Aufgabe9.ts
+++ Aufgabe9/Aufgabe9.ts
@@ -1,4 +1,4 @@
-/* Aufgabe 8: RandomBoxes
+/* Aufgabe : Erpresserbrief
 Name: Ngo, Thi Lien
 Matrikel: 256778
 Datum: 8.12.2017
@@ -8,22 +8,28 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 */
 
 
-namespace Aufgabe8 {
+namespace Aufgabe9 {
     window.addEventListener("load", init);
- 
+ //Globale Variable anlegen. -wie kann ich die versch. Div's vergleichen?
+let currentLetter: string;    
 
    function init(): void{
+       
        let alphabet: string [] = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z"]
        let anzahl: number = alphabet.length;
+       
+       
        
        if (anzahl == alphabet.length){
            for (let i: number = 0; i < alphabet.length; i++){
                drawRect(alphabet[i]);
                }
            }
+       drawKasten();
        }
     
-    
+    //handle click wird immmer aufgerufen, wenn mans anklickt 
+    //Event.target gibt dir das objekt zurück - liefert div zurück
     
  //Funktion Kästchen Zeichnen
     function drawRect (_alphabet: string): void {
@@ -34,12 +40,89 @@ namespace Aufgabe8 {
         div.style.width = "40px"; //Returns a String of an object
         div.style.height = "40px";
         div.style.backgroundColor = "red";
-        div.innerText = _alphabet;
+        div.innerText = _alphabet; // 
         div.style.marginLeft = "2px";
-        div.style.marginTop = "2px";
+        div.style.marginTop = "2px"; //
+        div.id = _alphabet;
+        div.className = "letters";
         
- 
+        div.addEventListener("click", handleClick);
         document.body.appendChild(div); //html ELement in den Code einfügen lassen - hängt div an body dran - wird gezeichnet
     }
-}
+    
+    function drawKasten(): void {
+            let letter: HTMLDivElement = document.createElement("div");
+        
+            letter.style.width = "800px"; //Returns a String of an object
+            letter.style.height = "300px";
+            letter.style.backgroundColor = "grey";
+            letter.style.marginLeft = "2px";
+            letter.style.marginTop = "2px"; //
+           
+            letter.addEventListener("click", putLetter);
+            document.body.appendChild(letter);
+        }
+    
+    function handleClick(_event: MouseEvent): void {
+            let click: HTMLDivElement = <HTMLDivElement>_event.target;
+            click.style.backgroundColor = "blue";
+            
+            currentLetter  = click.id;
+             
+            let divlist: NodeListOf<HTMLDivElement> = <NodeListOf<HTMLDivElement>> document.getElementsByClassName("letters");
+            
+            for (let i: number = 0; i < divlist.length; i++) {
+                if (currentLetter != divlist[i].id) {
+                divlist[i].style.backgroundColor = "blue";
+    
+                    }
+                }
+        }
+    /*function handleClick(_event: MouseEvent): void{
+        let click: HTMLDivElement = <HTMLDivElement>_event.target;
+        click.style.backgroundColor = "black";
+        //property vom click verändert = blacl´k
+        currentletter = click.id;
+        
+        let divlist: NodeListOf<HTMLDivElement> = <NodeListOf<HTMLDivElement>> document.getElementsByClassName("letters");
+        
+        // divlist ist von type nodelist of htmldiv element - hinter dem doppelpunkt: datentyp
+        // nodelistof ... wie ien array ... kann viele versch. datentyp speichern
+        //get element by clas name... 
+        
+        for(let i: number = 0; i<divlist.length; i++)<{
+         if(currentLetter != divlist[i].idy    //wenn der Buchstabe "currentletter" nicht gleich divlist ist der angeklickt ist
+            divlist[i].style.backgroundColor = "blue"; //dann wird der Buchstabe wieder blau
+       }
+    }
+    
+    */
+    
+   
+        
+  
+    
+        function putLetter(_event: MouseEvent): void {
+            let box: HTMLDivElement = document.createElement("div");
 
+            box.style.width = "30px";
+            box.style.height = "30px";
+            box.style.backgroundColor = "blue";
+            box.innerText = currentLetter;
+            box.style.marginLeft = "3px";
+            box.style.marginTop = "3px";
+            box.id = currentLetter;
+            box.className = "letters";
+            box.style.position = "absolute";
+            box.style.left = _event.pageX + "px";
+            box.style.top = _event.pageY + "px";
+            
+            
+            document.body.appendChild(box);
+
+            
+let clicking: HTMLDivElement = <HTMLDivElement>_event.target;  
+               
+        }
+    }
+  
